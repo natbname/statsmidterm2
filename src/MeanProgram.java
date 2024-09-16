8717ac0cef4189401591abc27199c817d7673fc3
troy <oconnort@go.stockton.edu>
2024-09-16 19:39:30 -0400

    troys comment

--- src/MeanProgram.java
+++ src/MeanProgram.java
@@ -8,6 +8,7 @@ public class MeanProgram
         for(int singleNum : listOfNumbers) //for each loop
         {
             sum = sum + singleNum;
+            //my name is troy and this is my comment
         }
         return sum /(double) listOfNumbers.size(); //casting double. also .length() is for arrays, .size() is for ArrayList
     }
