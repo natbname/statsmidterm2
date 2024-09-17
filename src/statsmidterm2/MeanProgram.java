e51b9f6b849b9d23f01fe9f31c7d794398e8570a
kirjorjos <kirjorjos@localhost>
2024-09-16 22:16:04 -0400

    added .gitingore, simplified operator in MeanProgram.java

--- src/statsmidterm2/MeanProgram.java
+++ src/statsmidterm2/MeanProgram.java
@@ -1,3 +1,4 @@
+package statsmidterm2;
 import java.util.ArrayList;
 
 public class MeanProgram 
--- src/statsmidterm2/TestMean.java
+++ src/statsmidterm2/TestMean.java
@@ -1,3 +1,4 @@
+package statsmidterm2;
 import java.util.ArrayList;
 
 //WRITTEN IN CLASS: not my code, using this for github assignment
